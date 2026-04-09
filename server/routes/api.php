4d8812519edc3d4441af93dc86a5a1f93b8a1086
fuad <nafisfuadisc@gmail.com>
2026-04-09 18:13:54 +0600

    add: created apis
    also updated environtent values for database

--- server/routes/api.php
+++ server/routes/api.php
@@ -1,8 +1,32 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/test', function () {
+    return 'Hello from Laravel';
+});
+
+Route::post('/names', function (Request $request) {
+    $name = $request->name;
+
+    $success = DB::insert('
+        insert into persons (name)
+        values (?);
+    ', [$name]);
+
+    return $success;
+});
+
+Route::get('/names', function () {
+    $names = DB::select('
+        select name from persons;
+    ');
+
+    return $names;
+});
